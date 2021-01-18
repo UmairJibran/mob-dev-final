3ba913cde5835f3c77f8c53f4d3f58c17840ce62
Umair Jibran <umairjibran7@gmail.com>
2021-01-18 16:34:05 +0500

    add backend config

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,8 @@ import 'package:final_paper/screens/ad_detail.dart';
 import 'package:final_paper/screens/login.dart';
 import 'package:flutter/material.dart';
 
+import '../dummy.dart';
+
 class Home extends StatefulWidget {
   static final String pageRoute = "/home";
 
@@ -39,54 +41,43 @@ class _HomeState extends State<Home> {
       body: Container(
         child: ListView.builder(
           itemBuilder: (_, index) {
-            var name = "Honda CD70";
-            var url =
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Honda_cd_70.jpg/300px-Honda_cd_70.jpg";
-            var price = '50000';
-            var location = 'Peshawar';
-            var description =
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua et dolore magna aliqu ";
-            var model = "2021";
-            var date = "2 jan 2021";
-            var mobile = "03120919647";
-            var bikeNumber = "FE-2193";
-            var views = 5000;
             return ListTile(
               onTap: () {
                 Navigator.of(context).pushNamed(
                   AdDetails.pageRoute,
                   arguments: {
-                    "name": name,
-                    "description": description,
-                    "price": price,
-                    "url": url,
-                    "location": location,
-                    "model": model,
-                    "date": date,
-                    "mobile": mobile,
-                    "views": views,
-                    "bikeNumber": bikeNumber,
+                    "name": ads[index]["name"],
+                    "description": ads[index]["description"],
+                    "price": ads[index]["price"],
+                    "url": ads[index]["url"],
+                    "location": ads[index]["location"],
+                    "model": ads[index]["model"],
+                    "date": ads[index]["date"],
+                    "mobile": ads[index]["mobile"],
+                    "views": ads[index]["views"],
+                    "bikeNumber": ads[index]["bikeNumber"],
                   },
                 );
               },
-              title: Text(name),
-              leading: ClipRRect(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(100),
+              title: Text(ads[index]["name"]),
+              leading: Container(
+                width: MediaQuery.of(context).size.width * 0.15,
+                height: MediaQuery.of(context).size.width * 0.15,
+                child: Image.network(
+                  ads[index]["url"],
                 ),
-                child: Image.network(url),
               ),
               subtitle: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Text(
-                    "PKR: ${price}",
+                    "PKR: ${ads[index]['price']}",
                     style: TextStyle(
                       color: Colors.green,
                       fontSize: 16,
                     ),
                   ),
-                  Text("${location}"),
+                  Text("${ads[index]['location']}"),
                 ],
               ),
               trailing: ClipRRect(
@@ -108,7 +99,7 @@ class _HomeState extends State<Home> {
               ),
             );
           },
-          itemCount: 20, //TODO: replace with length of the items
+          itemCount: ads.length,
         ),
       ),
     );
